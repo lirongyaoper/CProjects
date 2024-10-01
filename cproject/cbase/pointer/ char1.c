558207d4c000899977d6dda81de5a8476c105953
“lirongyaoper” <lirongyaoper@163.com>
2024-10-01 23:30:35 +0800

    241001001

--- cproject/cbase/pointer/ char1.c
+++ cproject/cbase/pointer/ char1.c
@@ -4,12 +4,12 @@
  *  Created on: Sep 24, 2024
  *      Author: lirongyaoper
  */
-#include <stdio.h>
-
-int main(){
-	int a= 0x11223344;
-	int* pa = &a;
-	*pa = 0;
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main(){
+//	int a= 0x11223344;
+//	int* pa = &a;
+//	*pa = 0;
+//	return 0;
+//}
 
