4ca17dd7336521977d22fcee0196bec1626bdf45
“lirongyaoper” <lirongyaoper@163.com>
2024-10-11 00:37:13 +0800

    241010001

--- cproject/cbase/pointer/string_len.c
+++ cproject/cbase/pointer/string_len.c
@@ -6,13 +6,42 @@
  */
 
 //
-//#include <stdio.h>
-//#include <string.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
 //
 //
-//
+//*****************************************************************************************
 //int main(){
+//	//strlen()   求字符串的长度，统计的是字符串中\0之前的字符的个数
 //	char arr[] =  "abcdefg";
-//	int len = my_strlen();
-//	printff("%d\n",len);
+//	int len = strlen(arr);
+//	printf("%d\n",len);
+//
+//	return 0;
 //}
+
+
+//*****************************************************************************************
+//int main(){
+//	//my_strlen()   求字符串的长度，统计的是字符串中\0之前的字符的个数
+//	char arr[] =  "abcdefgqwweeffg";
+//	int len = my_strlen(arr);
+//	printf("%d\n",len);
+//
+//	return 0;
+//}
+//
+////实现字符串个数统计函数
+//int my_strlen(const char* s){
+//	int count = 0 ;
+//	assert(s != NULL);
+//	while(*s != '\0'){
+//		count ++;
+//		s++;
+//	}
+//	return count;
+//}
+
+
+//*****************************************************************************************
