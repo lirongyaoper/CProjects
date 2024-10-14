6cd0ba426a8325e5e4ca5b59325e1b0c66234c3e
“lirongyaoper” <lirongyaoper@163.com>
2024-10-15 00:13:30 +0800

    241014001

--- cproject/cbase/pointer/Stringpointer/test1.c
+++ cproject/cbase/pointer/Stringpointer/test1.c
@@ -4,3 +4,18 @@
  *  Created on: Oct 12, 2024
  *      Author: lirongyaoper
  */
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+int main(){
+	int i = 0;
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int d = 40;
+	int *arr[4] = {&a,&b,&c,&d};
+	for(i = 0; i < 4;i++){
+		printf("%d ",);
+	}
+}
