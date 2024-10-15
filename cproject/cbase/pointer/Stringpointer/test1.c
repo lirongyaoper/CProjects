9e497c8c0493b3a8d962ec851252ff71216150d6
lirongyaoper <lirongyaoper@163.com>
2024-10-15 22:37:37 +0800

    241015001

--- cproject/cbase/pointer/Stringpointer/test1.c
+++ cproject/cbase/pointer/Stringpointer/test1.c
@@ -7,15 +7,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
-
-int main(){
-	int i = 0;
-	int a = 10;
-	int b = 20;
-	int c = 30;
-	int d = 40;
-	int *arr[4] = {&a,&b,&c,&d};
-	for(i = 0; i < 4;i++){
-		printf("%d ",);
-	}
-}
+//
+//int main(){
+//	int i = 0;
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int d = 40;
+//	int *arr[4] = {&a,&b,&c,&d};
+//	for(i = 0; i < 4;i++){
+//		printf("%d ",*arr[i]);
+//	}
+//	return 0;
+//}
