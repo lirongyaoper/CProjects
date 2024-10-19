56ee8756908d6004a90dc3450af4fc9267d32799
“lirongyaoper” <lirongyaoper@163.com>
2024-10-19 11:48:37 +0800

    201019001

--- cproject/cbase/pointer/1020pointer-4/test.c
+++ cproject/cbase/pointer/1020pointer-4/test.c
@@ -11,16 +11,26 @@
 #include <assert.h>
 
 //**************************************************************************************
-
-int main(){
-	const char * p = "hello world";
-	printf("%s\n",p);
-	printf("%s\n","hello world");
-	int lenstr = strlen(p);
-	int i = 0;
-	for(i = 0; i < lenstr; i++){
-		printf("%c" ,*(p+i));
-	}
-
-	return 0;
-}
+//
+//int main(){
+//	const char * p = "hello world";
+//	printf("%s\n",p);
+//	printf("%s\n","hello world");
+//	int lenstr = strlen(p);
+//	int i = 0;
+//	for(i = 0; i < lenstr; i++){
+//		printf("%c" ,*(p+i));
+//	}
+//
+//	return 0;
+//}
+//**************************************************************************************
+//int main(){
+//	char arr[] = "abcdefg";
+//	printf("%s\n", arr);//提供字符串的首地址，打印字符串
+//	char* p = arr;
+//	printf("%s" ,p);
+//	return 0;
+//
+//}
+//***********************************数组指针变量***************************************************
