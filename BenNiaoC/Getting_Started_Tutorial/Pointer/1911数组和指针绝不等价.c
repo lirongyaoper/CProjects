8e6ac8c2acb96815da55a7299b3b7ff715522b56
lirongyaoper <lirongyaoper@163.com>
2024-11-05 00:40:39 +0800

    2411050040

---
+++ BenNiaoC/Getting_Started_Tutorial/Pointer/1911数组和指针绝不等价.c
@@ -0,0 +1,16 @@
+/*
+ * 1911数组和指针绝不等价.c
+ *
+ *  Created on: Nov 5, 2024
+ *      Author: lirongyaoper
+ */
+#include <stdio.h>
+int main(){
+    int a[6] = {0, 1, 2, 3, 4, 5};
+    int *p = a;
+    int len_a = sizeof(a) / sizeof(int);
+    int len_p = sizeof(p) / sizeof(int);
+    printf("len_a = %d, len_p = %d\n", len_a, len_p);
+    return 0;
+}
+
